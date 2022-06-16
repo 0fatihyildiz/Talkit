d79870eb0fb063dd031f1c8a8ac04189cb19ea0a
Sacidev <sacidsen23@gmail.com>
2022-06-16 14:17:58 +0200

    my first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h1 class="text-3xl font-bold underline">Hello sacid!</h1>
 </template>
 
 <style lang="scss" scoped></style>
